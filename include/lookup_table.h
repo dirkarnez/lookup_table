f14bbbb0b4bb88e4dddd1860d6eedc8b9f14b97a
dirkarnez <smalldirkalex@gmail.com>
2020-11-30 19:26:45 +0800

    - init

---
+++ include/lookup_table.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#ifdef _WINDLL
+#define WIN_EXPORT __declspec(dllexport)
+#else
+#define WIN_EXPORT
+#endif
+
+template<int N>
+struct ArrayWrapper {
+    constexpr ArrayWrapper() : array() {
+        for (auto i = 0; i < N; i++)
+            array[i] = i; 
+    }
+    int array[N];
+};
+
+extern "C" WIN_EXPORT
+void GetRandomNumberArrayOfLength10(int* arr)
+{
+    constexpr auto length = 10;
+    auto array = ArrayWrapper<length>().array;
+    for (auto i = 0; i < length; i++)
+        arr[i] = array[i];
+}
